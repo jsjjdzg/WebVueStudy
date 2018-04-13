2b8cd2ead8aac799c862cc8d282a7f03801223fc
DZG-PC\DZG <jsjjdzg67@gmail.com>
2018-04-13 18:05:00 +0800

    20180413学习

--- day20180412/vueDemoC.js
+++ day20180412/vueDemoC.js
@@ -30,7 +30,40 @@ var app3 = new Vue({
     },
     methods: {
         addItem: function () {
-            this.items.push({message:'Too'})
+            this.items.push({ message: 'Too' });
+            this.object.fullName = this.object.fullName + "DZG"
+        },
+        addItem2: function () {
+            //app3.$set(app3.object,"fullName",'DZGDZGDZG')
+            Vue.set(app3.object, "fullName", 'DZGDZGDZG')
+        },
+        addItem3: function () {
+            app3.object = Object.assign({}, app3.object, {
+                firstName: 'JohnDZG',
+                lastName: 'DoeDZG',
+                age: 301
+            })
+        }
+    }
+})
+
+var app4 = new Vue({
+    el: "#app4",
+    data: {
+        keys : [1,2,3,4,5,6]
+    },
+    methods: {
+        even: function (numbers) {
+            return numbers.filter(function (number) {
+                return number % 2 === 0
+            })
+        }
+    },
+    computed: {
+        evenNumbers: function () {
+            return this.keys.filter(function (number) {
+                return number % 2 === 1
+            })
         }
     }
 })
