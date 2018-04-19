10e212df02b1c557e2ee4f7aded5cbaab7114ef6
DZG-PC\DZG <jsjjdzg67@gmail.com>
2018-04-19 18:03:36 +0800

    20180419提交

--- day20180417/vueDemoA.js
+++ day20180417/vueDemoA.js
@@ -2,6 +2,21 @@ Vue.component('app-component', {
     template:'<h2>Test App Component</h2>'
 })
 
+Vue.component('child', {
+    props: ['message','myMessage'],
+    template:'<span>{{message}} - {{myMessage}}</span>'
+})
+
+Vue.component('child2', {
+    props: ['parentmessage'],
+    template: '<span>{{parentmessage}}</span>'
+})
+
+Vue.component('child3', {
+    props: ['parentmessage1','parentmessage2'],
+    template: '<span>{{parentmessage1}} -- {{parentmessage2}}</span>'
+})
+
 var appComponent2 = {
     template:'<h3>Test App Component2</h3>'
 }
@@ -23,6 +38,21 @@ new Vue({
                     counter:0
                 }
             }
+        }
+    }
+})
+
+new Vue({
+    el: "#app3"
+})
+
+new Vue({
+    el: "#app4",
+    data: {
+        parent: '',
+        todo : {
+            parentmessage1: "¶¡Õý¸Õ",
+            parentmessage2: "¹Ë¶¬ºì",
         }
     }
 })
